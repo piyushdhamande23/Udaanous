d6eee4bb4202b932ac240922088ef1d4fba2085c
Piyush Dhamande <153407017+piyushdhamande23@users.noreply.github.com>
2025-04-16 11:24:35 +0530

    Add files via upload

---
+++ SQL/SQL(MIN,MAX,SUM,Count,AVG).sql
@@ -0,0 +1,37 @@
+
+select * from Emp1
+
+---MIN
+select min(Salary)  from Emp1
+
+
+--AVG
+
+select avg(Salary) from Emp1 
+
+--DIV
+select 10 div 2
+
+select Salary DIV 2 from Emp1
+
+--COUNT
+select count(Salary) from Emp1
+
+
+--SUM
+select Sum(Salary) from Emp1
+
+
+--ABS
+
+insert into Emp1 values (107,'Rahul','HR','Pune',-30000)
+
+select abs(-10)
+
+select abs (Salary) from Emp1
+
+
+--MAX
+
+select max(Salary) from Emp1
+
