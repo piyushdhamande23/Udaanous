99189edd9a6849203ade4208aa30aa433fab336c
Piyush Dhamande <153407017+piyushdhamande23@users.noreply.github.com>
2025-04-16 12:32:32 +0530

    Add files via upload

---
+++ SQL/SQL(SQRT,LOG,ABS,Count,MAX,MIN,ROUND).sql
@@ -0,0 +1,42 @@
+
+select * from Emp1
+
+--ABS
+select abs (-20)
+
+select ABS(Salary) from Emp1
+
+
+--MAX
+
+select max(Salary) from Emp1
+
+--MIN
+
+select min(Salary) from Emp1
+
+--COUNT
+select count(Salary) from Emp1
+
+
+--POWER 
+select power(Salary,2) from Emp1
+
+
+--SUM
+select sum(Salary) from Emp1
+
+
+--AVG
+select avg(Salary) from Emp1
+
+
+--ROUND
+select round(Salary,2) from Emp1
+
+
+--SQRT LOG
+
+select SQRT(Salary) from Emp1
+
+select Log(Salary) from Emp1
